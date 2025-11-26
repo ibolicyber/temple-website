f957725607f56fd40d83f956f05c720fe197fd3a
ibolicyber <ibrahimcyber4@gmail.com>
2025-11-26 22:46:52 +0400

    Remove Mobile Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,13 @@ if(navClose){
     })
 }
 
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
