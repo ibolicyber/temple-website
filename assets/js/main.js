d66ddcfb1f6054be9ad3832c047ddbe13575cba2
ibolicyber <ibrahimcyber4@gmail.com>
2025-12-02 15:53:27 +0400

    Blur Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,12 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scroolY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', blurHeader) 
+
